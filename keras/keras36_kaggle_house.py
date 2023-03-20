af037b9bd06d9a56e34135959e60fe058e05e219
seohee1205 <dbstjgml9621@naver.com>
2023-03-20 09:40:59 +0900

    AIA

--- keras/keras36_kaggle_house.py
+++ keras/keras36_kaggle_house.py
@@ -1,4 +1,3 @@
-from sklearn.datasets import fetch_covtype
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.models import Sequential, Model, load_model
 from tensorflow.python.keras.layers import Dense, Input, Dropout
@@ -26,141 +25,80 @@ test_csv = pd.read_csv(path + 'test.csv',
 print(test_csv)
 print(test_csv.shape)   # (1459, 79)
 
-# 결측치 제거
-print(train_csv.isnull().sum())     # LotFrontage
+#1-1. 결측치
+print(train_csv.isnull().sum())  
 
+df = pd.read_csv(path + 'train.csv')
+
+# 특정 열의 결측치를 특정 값으로 대체
+mean = df['LotFrontage'].mean()
+df['LotFrontage'] = df['LotFrontage'].fillna(mean)
+
+#1-2. 라벨인코딩
+le=LabelEncoder()
+for i in train_csv.columns:
+    if train_csv[i].dtype=='object':
+        train_csv[i] = le.fit_transform(train_csv[i])
+        test_csv[i] = le.fit_transform(test_csv[i])
+print(len(train_csv.columns))
+print(train_csv.info())
+train_csv=train_csv.dropna()
+print(train_csv.shape)
+
+
+#1-3. x, y 데이터 분리
 x = train_csv.drop(['SalePrice'], axis = 1)
-print(x)
+print(x.shape)
 y = train_csv['SalePrice']
-print(y)
+print(y.shape)
 
-# 데이터 분리
+#1-4. train, test 분리
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, train_size= 0.8, random_state= 789
+    x, y, train_size= 0.9, random_state= 888
 )
 
-print(x_train.shape, x_test.shape)      # (1168, 79) (292, 79)
-print(y_train.shape, y_test.shape)      # (1168,) (292,)
+print(x_train.shape, x_test.shape)      # (1008, 79) (113, 79)
+print(y_train.shape, y_test.shape)      # (1008,) (113,)
 
 
-##################################
-from sklearn.preprocessing import OrdinalEncoder
-
-print(np.unique(train_csv['MasVnrType']))
-
-
-'''
-data = pd.DataFrame({
-    'MSZoning': ['RH', 'RL', 'RM', 'FV', 'C (all)']
-    'Street': ['Pave', 'Grvl', 'apple'],
-    'Alley': ['NA', 'Pave', 'Grvl']
-    'LotShape': ['Reg', 'IR1', 'RM', 'FV', 'C (all)']
-    'LandContour': ['Lvl', 'HLS', 'Bnk', 'Low', 'C (all)']
-    'Utilities': ['AllPub', 'NoSeWa']
-    'LotConfig': ['Corner' 'CulDSac' 'FR2' 'FR3' 'Inside']
-    'LandSlope': ['Gtl' 'Mod' 'Sev']
-    'Neighborhood': ['Blmngtn' 'Blueste' 'BrDale' 'BrkSide' 'ClearCr' 'CollgCr' 'Crawfor'
-    'Edwards' 'Gilbert' 'IDOTRR' 'MeadowV' 'Mitchel' 'NAmes' 'NPkVill'
-    'NWAmes' 'NoRidge' 'NridgHt' 'OldTown' 'SWISU' 'Sawyer' 'SawyerW'
-    'Somerst' 'StoneBr' 'Timber' 'Veenker']
-    'Condition1': ['Artery' 'Feedr' 'Norm' 'PosA' 'PosN' 'RRAe' 'RRAn' 'RRNe' 'RRNn']
-    # 10개
-    
-    'Condition2': ['Artery' 'Feedr' 'Norm' 'PosA' 'PosN' 'RRAe' 'RRAn' 'RRNn']
-    'BldgType': ['1Fam' '2fmCon' 'Duplex' 'Twnhs' 'TwnhsE']
-    'HouseStyle': ['1.5Fin' '1.5Unf' '1Story' '2.5Fin' '2.5Unf' '2Story' 'SFoyer' 'SLvl']
-    'RoofStyle': 'Flat' 'Gable' 'Gambrel' 'Hip' 'Mansard' 'Shed']
-    'RoofMatl': ['ClyTile' 'CompShg' 'Membran' 'Metal' 'Roll' 'Tar&Grv' 'WdShake' 'WdShngl']
-    'Exterior1st': ['AsbShng' 'AsphShn' 'BrkComm' 'BrkFace' 'CBlock' 'CemntBd' 'HdBoard'
- 'ImStucc' 'MetalSd' 'Plywood' 'Stone' 'Stucco' 'VinylSd' 'Wd Sdng'
- 'WdShing']
-    'Exterior2nd': ['AsbShng' 'AsphShn' 'Brk Cmn' 'BrkFace' 'CBlock' 'CmentBd' 'HdBoard'
- 'ImStucc' 'MetalSd' 'Other' 'Plywood' 'Stone' 'Stucco' 'VinylSd'
- 'Wd Sdng' 'Wd Shng']
-   col26 'MasVnrType': []
-    'RoofMatl': []
-    'RoofMatl': []
-    ####### 10개
-    
-    'RoofMatl': []
-    'RoofMatl': []
-    
-    
-    
-    
-})
-'''
+#1-5. scaler
+# scaler = MinMaxScaler()
+# scaler.fit(x_train)                 # fit의 범위: x_train
+# x_train = scaler.transform(x_train)
+# x_test = scaler.transform(x_test) # x_train의 변화 비율에 맞춰하기 때문에 scaler에 fit을 할 필요가 없음(변환만 해줌)
+# # print(np.min(x_test), np.max(x_test))   # 0과 1이 나오는지 확인해
+# test_csv = scaler.transform(test_csv)   # test_csv에도 sclaer해줘야 함
 
 
+#2. 모델 
+input1 = Input(shape=(79,))
+dense1 = Dense(30)(input1)
+drop1 = Dropout(0.5)(dense1)
+dense2 = Dense(24, activation = 'relu')(drop1)
+dense3 = Dense(20, activation = 'relu')(dense2)
+dense4 = Dense(12)(dense3)
+dense5 = Dense(6, activation = 'relu')(dense4)
+dense6 = Dense(8, activation = 'relu')(dense5)
+output1 = Dense(1)(dense6)
+model = Model(inputs=input1, outputs=output1)
 
-
-
-# # 분리형을 수치형으로 바꿔주기
-# le = LabelEncoder()
-# le.fit(train_csv['MSSubClass'])   # 0과 1로 인정
-# aaa = le.transform(train_csv['MSSubClass'])
-# print(aaa)
-# print(type(aaa))    # <class 'numpy.ndarray'>
-# print(aaa.shape)    # (5497,) 벡터 형태
-# print(np.unique(aaa, return_counts= True))      # 몇 개씩 있는지    (array([0, 1]), array([1338, 4159], dtype=int64)) => 0이 1338개, 1이 4159개
-
-# train_csv['MSSubClass'] = aaa
-# print(train_csv)
-# test_csv['MSSubClass'] = le.transform(test_csv['MSSubClass'])
-
-
-
-
-'''
-scaler = RobustScaler()
-scaler.fit(x_train)                 # fit의 범위: x_train
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test) # x_train의 변화 비율에 맞춰하기 때문에 scaler에 fit을 할 필요가 없음(변환만 해줌)
-# print(np.min(x_test), np.max(x_test))   # 0과 1이 나오는지 확인해
-
-test_csv = scaler.transform(test_csv)   # test_csv에도 sclaer해줘야 함
-
-
-
-# #2. 모델 
-model = Sequential()
-model.add(Dense(20, input_dim = 8))
-model.add(Dropout(0.3))
-model.add(Dense(18, activation = 'relu'))
-model.add(Dropout(0.2))
-model.add(Dense(12, activation = 'relu'))
-model.add(Dropout(0.5))
-model.add(Dense(8, activation = 'relu'))
-model.add(Dense(6, activation = 'relu'))
-model.add(Dense(1, activation = 'linear'))
 
 #3. 컴파일, 훈련
 model.compile(loss = 'mse', optimizer = 'adam',
-              metrics= ['accuracy'])
-
-import datetime
-date = datetime.datetime.now()
-print(date)  # 2023-03-14 11:11:30.046663
-date = date.strftime("%m%d_%H%M") # 시간을 문자로 (월, 일, 시간, 분)
-print(date)  # 0314_1116
-
-filepath = './_save/MCP/keras28/'
-filename = '{epoch:04d}-{val_loss:.4f}.hdf5' # epoch의 4번째 정수까지, val-loss의 4번째 소수까지
+              metrics= ['acc'])
 
 # 정의하기
-es = EarlyStopping(monitor = 'val_accuracy', patience = 35, mode = 'auto',
+es = EarlyStopping(monitor = 'val_loss', patience = 80, mode = 'min',
                    verbose = 1,
                     restore_best_weights = True)
-
-mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto',
-        verbose = 1, 
-        save_best_only= True,
-        filepath="".join([filepath, 'k27_', date, '_', filename]))
-
-hist = model.fit(x_train, y_train, epochs = 5000, batch_size = 30,
+# mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto',
+#         verbose = 1, 
+#         save_best_only= True,
+#         filepath="".join([filepath, 'k27_', date, '_', filename]))
+hist = model.fit(x_train, y_train, epochs = 2000, batch_size = 77,
           validation_split = 0.2,
           verbose = 1,
-          callbacks = [es, mcp])
+          callbacks = [es])
 
 
 #4. 평가, 예측
@@ -168,19 +106,26 @@ loss = model.evaluate(x_test, y_test)
 print('loss : ', loss)
 
 y_predict = model.predict(x_test)
+
 r2 = r2_score(y_predict, y_test)
 print('r2 스코어 : ', r2)
 
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
-
 rmse = RMSE(y_test, y_predict)              # RMSE 함수 사용
 print("RMSE : ", rmse)
 
+# 시간 측정
+import datetime
+date = datetime.datetime.now()
+date = date.strftime('%m%d_%H%M%S')
+
+y_submit = model.predict(test_csv)
+y_submit = pd.DataFrame(y_submit)
+y_submit = y_submit.fillna(y_submit.mean())
+y_submit = np.array(y_submit)
+
 # 파일 생성
 submission = pd.read_csv(path + 'sample_submission.csv', index_col = 0)
-
-path_save = './_save/kaggle_house/'
-submission.to_csv(path_save + 'submit_0317_0845.csv')
-
-'''
+submission['SalePrice'] = y_submit
+submission.to_csv(path_save + 'submit_0320_0933.csv')
