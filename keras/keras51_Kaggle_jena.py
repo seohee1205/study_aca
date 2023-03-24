38b23c004705f173e3d534fb6d33d048000709bf
seohee1205 <dbstjgml9621@naver.com>
2023-03-24 18:47:18 +0900

    AIA

--- keras/keras51_Kaggle_jena.py
+++ keras/keras51_Kaggle_jena.py
@@ -5,7 +5,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.models import Sequential, Model
 from tensorflow.python.keras.layers import Dense, Conv1D, Input, Flatten
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
@@ -36,43 +36,55 @@ print(datasets['T (degC)'].to_numpy())  # 판다스를 넘파이로
 # plt.plot(datasets['T (degC)'].values)
 # plt.show()
 
-dataset = np.array(range(1, 420551))
-timesteps = 6
-
-# 입력 데이터와 출력 데이터 생성
-input_data, output_data = [], []
-values = datasets.values
-for i in range(len(values) - timesteps):
-    input_data.append(values[i:i + timesteps, 1:])
-    output_data.append(values[i + timesteps, 1:])
-
-# 입력 데이터와 출력 데이터를 numpy array로 변환
-input_data = np.array(input_data)
-output_data = np.array(output_data)
-
-# 입력 데이터와 출력 데이터 shape 출력
-print('Input Data Shape:', input_data.shape)    # (420407, 144, 13)
-print('Output Data Shape:', output_data.shape)  # (420407, 13)
-
+ 
 # x, y 분리
 x = datasets.drop(['T (degC)'], axis = 1)
 y = datasets['T (degC)']
 
 print(x.shape, y.shape)         # (420551, 13) (420551,)
 
+
+
 # train, test, predict 분리
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, shuffle=False, train_size=0.7, random_state=777)
 x_test, x_predict, y_test, y_predict = train_test_split(
-    x_test, y_test, shuffle=False, train_size=0.1, random_state=777)
+    x_test, y_test, shuffle=False, train_size=0.67, random_state=777)
 
-print(x_train.shape, y_train.shape)    # (294385, 13), (294385,)
-print(x_test.shape, y_test.shape)      # (42056, 13), (42056,)
-print(x_predict.shape, y_predict.shape)# (126110, 13), (126110,)
+
+print(x_train.shape, y_train.shape)     # (294385, 13), (294385,)
+print(x_test.shape, y_test.shape)       # 84531, 13) (84531,)
+print(x_predict.shape, y_predict.shape) # (41635, 13) (41635,)
+
+
+dataset = np.array(range(1, 420551))
+timesteps = 6
+
+
+def split_x(dataset, timesteps):
+    aaa = []    # aaa라는 빈 리스트를 만들어라
+    for i in range(len(dataset) - timesteps):        
+        subset = dataset[i : (i + timesteps)]               
+        aaa.append(subset)                                  
+    return np.array(aaa)    
+
+x_train = split_x(x_train, timesteps)
+x_test = split_x(x_test, timesteps)
+x_predict = split_x(x_predict, timesteps)
+
+y_train = y_train[timesteps:]
+y_test = y_test[timesteps:]
+y_predict = y_predict[timesteps:]
+
+
+print(x_train.shape, y_train.shape)         # (294379, 6, 13) (294379,)
+print(x_test.shape, y_test.shape)           # (84525, 6, 13) (84525,)
+print(x_predict.shape, y_predict.shape)     # (41629, 6, 13) (41629,)
+
 
 
 #2. 모델 구성
-input1 = Input(shape=(13, 1))
+input1 = Input(shape=(6, 13))
 Conv1 = Conv1D(64, 2, activation='linear')(input1)
 Conv2 = Conv1D(26, 2, activation='relu')(Conv1)
 Flat1 = Flatten()(Conv2)
@@ -87,7 +99,7 @@ model = Model(inputs=input1, outputs=output1)
 start = time.time()
 model.compile(loss = 'mse', optimizer = 'adam', metrics = ['mae'])
 
-es = EarlyStopping(monitor = 'val_loss', patience = 5, mode = 'auto',
+es = EarlyStopping(monitor = 'val_loss', patience = 100, mode = 'auto',
                    verbose = 1, restore_best_weights= True)
 
 # mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto',
@@ -95,7 +107,7 @@ es = EarlyStopping(monitor = 'val_loss', patience = 5, mode = 'auto',
 #         save_best_only= True,
 #         filepath="".join([filepath, 'k27_', date, '_', filename]))
 
-model.fit(x_train, y_train, epochs = 2, batch_size = 64,
+model.fit(x_train, y_train, epochs = 2000, batch_size = 777,
           validation_split = 0.2,
           verbose = 1,
           callbacks = [es])
@@ -103,10 +115,11 @@ model.fit(x_train, y_train, epochs = 2, batch_size = 64,
 end = time.time()
 
 #4. 평가, 예측
-result = model.evaluate(x_test, y_test)
-print('result : ', result)
+loss = model.evaluate(x_test, y_test)
+print('loss : ', loss)
 
-y_predict = model.predict(x_predict)
+y_predict = model.predict(x_test)
+
 r2 = r2_score(y_test, y_predict)
 print('r2 스코어 : ', r2)
 
@@ -117,4 +130,6 @@ rmse = RMSE(y_test, y_predict)              # RMSE 함수 사용
 print("RMSE : ", rmse)
 
 print('걸린 시간 : ', np.round(end-start, 2))
+
+
 
