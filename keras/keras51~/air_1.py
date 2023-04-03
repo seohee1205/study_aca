93b2359491c60263c30458ffa8a000b143fee1df
seohee1205 <dbstjgml9621@naver.com>
2023-04-03 20:10:28 +0900

    AIA

--- keras/keras51~/air_1.py
+++ keras/keras51~/air_1.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sklearn.neighbors import LocalOutlierFactor
+import datetime
+date = datetime.datetime.now()
+date = date.strftime('%m%d_%H%M%S')
 
 # Load train and test data
 path='d:/study_data/_data/air/dataset/'
@@ -12,7 +15,7 @@ submission = pd.read_csv(path+'answer_sample.csv')
 # data = pd.concat([train_data, test_data], axis=0)
 data = train_data
 # Preprocess data
-# ...
+# 전처리 필요하면 전처리 하기
 
 # Train Local Outlier Factor model on train data
 model = LocalOutlierFactor(n_neighbors=20, contamination=0.05, novelty=True)
@@ -25,4 +28,5 @@ predictions = model.predict(test_data)
 # Save predictions to submission file
 new_predictions = [0 if x == 1 else 1 for x in predictions]
 submission['label'] = pd.DataFrame({'Prediction': new_predictions})
-submission.to_csv(save_path+'submission.csv', index=False)
+submission.to_csv(save_path + date + 'submission.csv', index=False)
+
