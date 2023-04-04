c24acf18a32f2fc7f5f29102c36135b1e4b98bae
seohee1205 <dbstjgml9621@naver.com>
2023-04-04 12:12:54 +0900

    AIA

--- keras/keras51~/keras59_1_Tokenizer.py
+++ keras/keras51~/keras59_1_Tokenizer.py
@@ -22,13 +22,18 @@ print(type(x))  # <class 'list'>
 # print(x)
 # print(x.shape)      # (1, 11, 9)
 
-##### 2. get_dummies #####
+##### 2. get_dummies #####          1차원으로 받아들임
 import pandas as pd
 # 리스트를 넘파이로 바꾸기
 import numpy as np
 # x = pd.get_dummies(np.array(x).reshape(11,))
 x = pd.get_dummies(np.array(x).ravel())     # 윗줄이랑 똑같음 
 print(x)
+print(x.shape)
 
-# 왜 리스트는 안 되는지
-# 
+##### 3. 사이킷런 onehot #####      2차원으로 받아들임
+# from sklearn.preprocessing import OneHotEncoder
+# ohe = OneHotEncoder()
+# x = ohe.fit_transform(np.array(x).reshape(-1, 1)).toarray()
+# print(x)
+# print(x.shape)      # (11, 8)
