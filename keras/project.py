e4d2133b24e52237b4942913c661588ea91769a5
seohee1205 <dbstjgml9621@naver.com>
2023-04-11 11:11:37 +0900

    project

--- keras/project.py
+++ keras/project.py
@@ -4,13 +4,13 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
 import keras.backend as K
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, BatchNormalization, MaxPooling2D, Dropout
+from tensorflow.keras.layers import Conv2D, LeakyReLU, Flatten, Dense, BatchNormalization, MaxPooling2D, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 
 
 # sigmoid는 0부터 1까지인데 감정을 0부터 3까지 정의했으니까 사용자 정의 함수를 사용해야함
 def custom_activation(x):
-    return K.clip(x, 0, 3)
+    return K.clip(x, 0, 3,)      # x를 0부터 3까지 사이의 값을 반환하겠다
 
 
 custom_activation.name = 'custom_activation'
@@ -36,75 +36,126 @@ shuffle=True
 xy_test = test_datagen.flow_from_directory(
  'd:/study_data/_data/project/test/',
 target_size=(48, 48),
-batch_size=batch_size,
+batch_size= batch_size,
 class_mode='sparse',
 color_mode='grayscale',
-shuffle=True
+shuffle= False
 )
 # Found 5085 images belonging to 4 classes.
 
 # 모델 구성
 
-num_classes = 4
-num_detectors = 32
-width, height = 48, 48
-
 model = Sequential()
-model.add(Conv2D(filters=num_detectors, kernel_size=3, activation='swish', padding='same', input_shape=(width, height, 3)))
+model.add(Conv2D(128, (2, 2), padding='same', input_shape=(48, 48, 1), activation= 'relu'))
 model.add(BatchNormalization())
-model.add(Dropout(0.2))
-model.add(Conv2D(2*num_detectors, 3, activation='swish', padding='same'))
-model.add(BatchNormalization())
-model.add(Conv2D(2*num_detectors, 3, activation='swish', padding='same'))
+model.add(Conv2D(80, (2, 2), padding='same', activation= 'relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
-model.add(Conv2D(2*2*num_detectors, 3, activation='swish', padding='same'))
+model.add(Conv2D(60, (2, 2), padding='same', activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(100, (2, 2), padding='same', activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(120, (2, 2), padding='same', activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.1))
+model.add(Conv2D(90, (3, 3), padding='same', activation= 'relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(2*2*num_detectors, activation='swish'))
+model.add(Dense(64, activation= 'relu'))
+model.add(Dense(32, activation= 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation= 'relu'))
+model.add(Dense(50, activation= 'relu'))
+model.add(Dropout(0.2))
 model.add(Dense(1, activation=lambda x: custom_activation(x)))
-# model.summary()
+model.summary()
+
 
 
 # 컴파일, 훈련
-model.compile(loss= 'mse', optimizer= 'adam', metrics= ['acc'])
+model.compile(loss= 'mse', optimizer= 'adam')
 
-es = EarlyStopping(monitor = 'acc', patience = 60, mode = 'auto',
+es = EarlyStopping(monitor = 'val_loss', patience = 50, mode = 'min',
                    verbose = 1, restore_best_weights= True)
 
-model.fit_generator(xy_train, epochs= 70,
+model.fit(xy_train, epochs= 100,
+                    validation_data= xy_test,
+                    shuffle = True,
                     # steps_per_epoch= 10,
                     # validation_data = xy_test,
                     # validation_steps = 5085/batch_size,
-                    callbacks = [es])
+                    callbacks = [es]
+                    )
 
 
-# def food(y):
-#     for i in range(len(y)):
-#         if 0<=y[i]<3/8:
-#             print('emotion : angry, food recommendation : a')
-#         elif 3/8<=y[i]<3/4:
-#             print('emotion : angry, food recommendation : b')
-#         elif 3/4<=y[i]<9/8:
-#             print('emotion : sad, food recommendation : c')
-#         elif 9/8<=y[i]<12/8:
-#             print('emotion : sad, food recommendation : d')
-#         elif 12/8<=y[i]<15/8:
-#             print('emotion : default, food recommendation : e')
-#         elif 15/8<=y[i]<18/8:
-#             print('emotion : default, food recommendation : f')
-#         elif 18/8<=y[i]<21/8:
-#             print('emotion : happy, food recommendation : g')
-#         elif 21/8<=y[i]<=3:
-#             print('emotion : happy, food recommendation : h')
+def food(y):
+    for i in range(len(y)):
+        if y[i]<1/8:
+            print('emotion : angry, food recommendation : 신길동 매운짬뽕')
+        elif 1/8<=y[i]<2/8:   
+            print('emotion : angry, food recommendation : 디진다 돈까스')
+        elif 2/8<=y[i]<3/8:
+            print('emotion : angry, food recommendation : 송주불냉면')
+        elif 3/8<=y[i]<4/8:
+            print('emotion : angry, food recommendation : 치킨플러스 - 핵매운치킨') 
+        elif 4/8<=y[i]<5/8:
+            print('emotion : angry, food recommendation : 염라대왕라면') 
+        elif 5/8<=y[i]<6/8:
+            print('emotion : angry, food recommendation : 엽기떡볶이')    
+            
+            
+        elif 3/4<=y[i]<7/8:
+            print('emotion : sad, food recommendation : 닭발')
+        elif 7/8<=y[i]<8/8:
+            print('emotion : sad, food recommendation : 낙지김치죽')    
+        elif 8/8<=y[i]<9/8:
+            print('emotion : sad, food recommendation : 짬뽕')    
+        elif 9/8<=y[i]<10/8:
+            print('emotion : sad, food recommendation : 매운돼지갈비찜')    
+        elif 11/8<=y[i]<12/8:
+            print('emotion : sad, food recommendation : 제육덮밥')    
+        elif 12/8<=y[i]<13/8:
+            print('emotion : sad, food recommendation : 안성탕면')    
+            
+            
+            
+        elif 13/8<=y[i]<14/8:
+            print('emotion : default, food recommendation : 비빔냉면')
+        elif 14/8<=y[i]<15/8:
+            print('emotion : default, food recommendation : 카레')    
+        elif 16/8<=y[i]<17/8:
+            print('emotion : default, food recommendation : 국밥')    
+        elif 18/8<=y[i]<19/8:
+            print('emotion : default, food recommendation : 피자')    
+        elif 20/8<=y[i]<21/8:
+            print('emotion : default, food recommendation : 짜장면')    
+        elif 22/8<=y[i]<23/8:
+            print('emotion : default, food recommendation : d')    
+            
+            
+ 
+        elif 23/8<=y[i]<24/8:
+            print('emotion : happy, food recommendation : 소고기')
+        elif 24/8<=y[i]<25/8:
+            print('emotion : happy, food recommendation : 육회비빔밥')    
+        elif 25/8<=y[i]<26/8:
+            print('emotion : happy, food recommendation : 돈까스')    
+        elif 27/8<=y[i]<28/8:
+            print('emotion : happy, food recommendation : 쌀국수')    
+        elif 28/8<=y[i]<29/8:
+            print('emotion : happy, food recommendation : 김밥')    
+        elif 29/8<=y[i]:
+            print('emotion : happy, food recommendation : 죽')
 
 
-# import random
-# random_index_1 = np.random.randint(0, 19)
-# random_index_2 = np.random.randint(0, 19)
-# x_pred = xy_test[random_index_1][0][random_index_2].reshape(-1, 100, 100, 1)
-# food(model.predict(x_pred))
+import random
+random_index_1 = np.random.randint(0, 19)
+random_index_2 = np.random.randint(0, 19)
+x_pred = xy_test[random_index_1][0][random_index_2].reshape(-1, 48, 48, 1)
+food(model.predict(x_pred))
+
 
 # 평가, 예측
-acc = model.evaluate(xy_test)[1]
-print('acc : ', acc)
+loss = model.evaluate(xy_test)
+print('loss : ', loss)
--- keras/코드.py
+++ keras/코드.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 from PIL import Image
@@ -17,7 +18,7 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 batch_size = 20
 
 xy_train = train_datagen.flow_from_directory(
-'c:/study/_data/sh/train/',
+'d:/study_data/_data/project/train/',
 target_size=(100, 100),
 batch_size=batch_size,
 class_mode='sparse',
@@ -27,22 +28,10 @@ shuffle=True
 
 
 xy_test = test_datagen.flow_from_directory(
-'c:/study/_data/sh/test/',
+'d:/study_data/_data/project/test/',
 target_size=(100, 100),
 batch_size=batch_size,
 class_mode='sparse',
 color_mode='grayscale',
 shuffle=True
 )
-
-model = Sequential()
-model.add(Conv2D(8, (2, 2), input_shape=(100, 100, 1), activation='relu'))
-model.add(Conv2D(8, (3, 3), activation='relu'))
-model.add(Flatten())
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation=lambda x: custom_activation(x)))
-
-model.compile(loss='mse', optimizer='adam')
-model.summary()
-
-model.fit_generator(xy_train, epochs=10, steps_per_epoch=10, verbose=1,)
