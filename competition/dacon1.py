af0a39e167e8e29b2e1d30c8b608cec060058b84
seohee1205 <dbstjgml9621@naver.com>
2023-04-22 20:56:40 +0900

    230422

--- competition/dacon1.py
+++ competition/dacon1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
 from sklearn.metrics import mean_squared_error
@@ -27,15 +27,12 @@ print(test_csv.shape)   # (7500, 9)
 # print(train_csv.isnull().sum())   # 결측치 없음
 
 #1-4. 라벨인코더
-encoder1 = LabelEncoder()
-encoder2 = LabelEncoder()
-train_csv['Weight_Status'] = encoder1.fit_transform(train_csv['Weight_Status'])
+encoder = LabelEncoder()
 
-train_csv['Gender'] = encoder2.fit_transform(train_csv['Gender'])
-
-test_csv['Weight_Status'] = encoder1.fit_transform(test_csv['Weight_Status'])
-
-test_csv['Gender'] = encoder2.fit_transform(test_csv['Gender'])
+train_csv['Weight_Status'] = encoder.fit_transform(train_csv['Weight_Status'])
+train_csv['Gender'] = encoder.fit_transform(train_csv['Gender'])
+test_csv['Weight_Status'] = encoder.fit_transform(test_csv['Weight_Status'])
+test_csv['Gender'] = encoder.fit_transform(test_csv['Gender'])
 
 #1-3. x, y 분리
 x = train_csv.drop(['Calories_Burned'], axis= 1)
@@ -60,26 +57,28 @@ test_csv = scaler.transform(test_csv)
 
 #2. 모델 구성
 model = Sequential()
-model.add(Dense(256, input_dim = 9))
-model.add(Dense(128, activation = 'selu'))
-model.add(Dropout(0.2))
-model.add(Dense(128, activation = 'selu'))
-model.add(Dense(64, activation = 'selu'))
-model.add(Dense(80, activation = 'selu'))
-model.add(Dropout(0.1))
-model.add(Dense(64, activation = 'selu'))
+model.add(Dense(128, input_dim = 9))
 model.add(Dense(64, activation = 'selu'))
 model.add(Dropout(0.1))
+model.add(BatchNormalization())
+model.add(Dense(128, activation = 'selu'))
+model.add(Dense(64, activation = 'selu'))
+model.add(Dropout(0.1))
+model.add(Dense(64, activation = 'selu'))
+model.add(BatchNormalization())
+model.add(Dense(32, activation = 'selu'))
+model.add(BatchNormalization())
+model.add(Dense(32, activation = 'selu'))
 model.add(Dense(32, activation = 'selu'))
 model.add(Dense(16, activation = 'selu'))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
 model.compile(loss = 'mse', optimizer = 'adam')
-es = EarlyStopping(monitor = 'val_loss', patience = 50, mode = 'min',
+es = EarlyStopping(monitor = 'val_loss', patience = 80, mode = 'min',
                    verbose = 1)
 
-model.fit(x_train, y_train, epochs = 1000, batch_size = 24,
+model.fit(x_train, y_train, epochs = 2000, batch_size = 24,
           validation_split= 0.2,
           verbose = 1,
           callbacks = [es])
@@ -105,5 +104,3 @@ submission = pd.read_csv(path + 'sample_submission.csv')
 submission['Calories_Burned'] = y_submit
 
 submission.to_csv(path_save + date + 'sample_submission.csv', index=False)
-
-
