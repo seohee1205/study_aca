7ab1316761d26b35ce14231ae507cf09837923e6
seohee1205 <dbstjgml9621@naver.com>
2023-04-24 13:15:09 +0900

    .

--- ml/ml21~30/m28_FI_01_iris.py
+++ ml/ml21~30/m28_FI_01_iris.py
@@ -4,7 +4,6 @@
 # 모델을 돌려서 결과 도출
 # 기초모델들과 성능비교
 
-
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris, load_breast_cancer, load_digits, load_wine
@@ -13,12 +12,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from xgboost import XGBClassifier
+import xgboost as xgb
 
 #1. 데이터
 
 data_list = [load_iris(), load_breast_cancer(), load_digits(), load_wine()]
 model_list = [DecisionTreeClassifier(), RandomForestClassifier(), GradientBoostingClassifier(), XGBClassifier(objective='multi:softmax')]
-# scaler_list = [MinMaxScaler(), StandardScaler(), RobustScaler(), MaxAbsScaler()] 
 
 for i in range(len(data_list)):
     data = data_list[i]
@@ -28,31 +27,37 @@ for i in range(len(data_list)):
     )
 
     for j, model in enumerate(model_list): 
-            # 모델 훈련
-            model.fit(x_train, y_train)
+        # 모델 훈련
+        model.fit(x_train, y_train)
 
-            # feature importance 계산
-            feature_importance = model.feature_importances_
-            feature_importance_percent = feature_importance / feature_importance.sum()
+        # feature importance 계산
+        feature_importance = model.feature_importances_
+        feature_importance_percent = feature_importance / feature_importance.sum()
 
-            # 중요도가 낮은 컬럼 제거
-            threshold = np.percentile(feature_importance_percent, 25) # 하위 25%
-            feature_idx = np.where(feature_importance_percent >= threshold)[0]
-            selected_x_train = x_train[:, feature_idx]
-            selected_x_test = x_test[:, feature_idx]
+        # 중요도가 낮은 컬럼 제거
+        threshold = np.percentile(feature_importance_percent, 25) # 하위 25%
+        feature_idx = np.where(feature_importance_percent >= threshold)[0]
+        selected_x_train = x_train[:, feature_idx]
+        selected_x_test = x_test[:, feature_idx]
 
-            # 모델 훈련 후 정확도 계산
+        # XGBClassifier 모델에서 num_class 파라미터 추가 
+        if isinstance(model, XGBClassifier):
+            model.fit(selected_x_train, y_train, n_classes=len(np.unique(y)))
+        else:
             model.fit(selected_x_train, y_train)
-            train_acc = model.score(selected_x_train, y_train)
-            test_acc = model.score(selected_x_test, y_test)
+            
+        train_acc = model.score(selected_x_train, y_train)
+        test_acc = model.score(selected_x_test, y_test)
 
-            # 결과 출력
-            print(f"Dataset: {data_list[i].DESCR.splitlines()[0]}")
-            print(f"Model: {type(model).__name__}")
-            print(f"Selected Features: {len(feature_idx)} / {x_train.shape[1]}")
-            print(f"Train Accuracy: {train_acc:.4f}")
-            print(f"Test Accuracy: {test_acc:.4f}")
-            print("=" * 50)
+        # 결과 출력
+        print(f"Dataset: {data_list[i].DESCR.splitlines()[0]}")
+        print(f"Model: {type(model).__name__}")
+        print(f"Selected Features: {len(feature_idx)} / {x_train.shape[1]}")
+        print(f"Train Accuracy: {train_acc:.4f}")
+        print(f"Test Accuracy: {test_acc:.4f}")
+        print("=" * 50)
+
+
 
 
 
