9b3b9ff78d47c0c47d9ac2533ed1fbc70c6715fd
seohee1205 <dbstjgml9621@naver.com>
2023-04-25 14:20:43 +0900

    230425

---
+++ ml/ml21~30/m34_LDA_classifier.py
@@ -0,0 +1,79 @@
+# Linear Discriminant Analysis
+# 선형판별분석
+# 주어진 데이터를 가장 잘 구분하는 초평면을 찾아내는 방법
+# 이 초평면은 클래스 간 분산(between-class variance)과 클래스 내 분산(within-class variance)을
+# 최대화, 최소화 하는 방식으로 결정됨
+# LDA는 주로 분류 문제에서 사용되며, 입력 변수들이 주어졌을 때 
+# 그 입력 변수들이 어떤 클래스에 속하는지 예측하는 모델을 만들 때 사용됨 
+# 이를 위해서 LDA는 주어진 데이터 셋에서 클래스 간 분산과 클래스 내 분산을 추정하여 최적의 결정 경계를 찾아냄
+# LDA는 이진 분류 뿐 아니라 다중 클래스 분류 문제에서도 사용할 수 있음
+
+
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris, load_breast_cancer, load_digits, load_wine, fetch_covtype
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+'''
+#1. 데이터
+# x, y = load_iris(return_X_y=True)
+# x, y = load_breast_cancer(return_X_y=True)        # (569, 1)
+x, y = load_digits(return_X_y=True)                 # (1797, 9)
+
+# pca = PCA(n_components=3)
+## pca = PCA()
+# x = pca.fit_transform(x)
+print(x.shape)      # (150, 64)
+
+# lda = LinearDiscriminantAnalysis()
+lda = LinearDiscriminantAnalysis(n_components=3)
+# n_components는 클래스의 개수 빼기 하나 이하로 가능하다
+
+x_lda = lda.fit_transform(x, y)
+print(x_lda.shape)      # (150, 2)
+'''
+# [실습]
+
+data_name_list = [load_iris(return_X_y= True), load_breast_cancer(return_X_y= True), 
+            load_digits(return_X_y=True), load_wine(return_X_y= True), fetch_covtype(return_X_y=True)]
+
+data_name = ["load_iris", "load_breast_cancer", "load_digits", "load_wine", "fetch_covtype"]
+
+
+for i, v in enumerate(data_name_list):
+    x, y = v
+    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state= 337)
+
+    print('=================', '변환 전', data_name[i], '===============')
+    # 모델 구성
+    model = RandomForestClassifier(random_state=337)
+    # 컴파일, 훈련
+    model.fit(x_train, y_train)
+    # 평가, 예측
+    result = model.score(x, y)
+    print("acc :", result)
+
+    print('=================', '변환 후', data_name[i], '===============')
+    lda = LinearDiscriminantAnalysis()
+    x_lda = lda.fit_transform(x, y)
+    
+    x_train1, x_test1, y_train1, y_test1 = train_test_split(x, y, train_size=0.8, random_state= 337)
+    print(data_name[i], ':', x.shape, '->', x_lda.shape)
+    
+    # 모델 구성
+    model = RandomForestClassifier(random_state=337)
+    # 컴파일, 훈련 
+    model.fit(x_train1, y_train1)
+    # 평가, 예측
+    result1 = model.score(x_test1, y_test1)
+    print("acc :", result1)
+    
+
+
+
+
--- ml/ml21~30/m37_LDA_EVR.py
+++ ml/ml21~30/m37_LDA_EVR.py
@@ -7,17 +7,71 @@ from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_iris, load_breast_cancer, load_digits
+from sklearn.datasets import load_iris, load_breast_cancer, load_digits, load_wine, fetch_covtype
 from tensorflow.keras.datasets import cifar100
 
+'''
 #1. 데이터
-# x, y = load_iris(return_X_y=True)
+x, y = load_iris(return_X_y=True)       # (150, 4) -> (150, 2)
+# [0.9912126 1.       ]
+
 # x, y = load_breast_cancer(return_X_y=True)  
-x, y = load_digits(return_X_y=True)          
+# x, y = load_digits(return_X_y=True) 
+# x, y = load_wine(return_X_y=True) 
+# x, y = fetch_covtype(return_X_y=True) 
 
 lda = LinearDiscriminantAnalysis()
 # n_components는 클래스의 개수 빼기 하나 이하로 가능하다    
 
 x_lda = lda.fit_transform(x, y)
-print(x_lda.shape)      # (50000, 98)       or 
+print(x_lda.shape)      # (1797, 9)
 
+lda_EVR = lda.explained_variance_ratio_
+
+cumsum = np.cumsum(lda_EVR)
+print(cumsum)
+'''
+data_list = [load_iris, load_breast_cancer, 
+            load_digits, load_wine, fetch_covtype]
+# data_list = [load_iris(return_X_y= True), load_breast_cancer(return_X_y= True), 
+#             load_digits(return_X_y=True), load_wine(return_X_y= True), fetch_covtype(return_X_y=True)]
+data_name = ["load_iris", "load_breast_cancer", "load_digits", "load_wine", "fetch_covtype"]
+
+for i, value in enumerate(data_list):
+    print('===============================',data_name[i],'===============================')
+    x, y = value(return_X_y=True)
+    print('x_shape :', x.shape)
+    lda = LinearDiscriminantAnalysis()
+    x_lda = lda.fit_transform(x, y)
+    print('x_lda의 shape :', x_lda.shape)
+    lda_EVR = lda.explained_variance_ratio_
+    cumsum = np.cumsum(lda_EVR)
+    print(cumsum)
+print('====================================================')  
+
+
+# =============================== load_iris ===============================
+# x_shape : (150, 4)
+# x_lda의 shape : (150, 2)
+# [0.9912126 1.       ]
+# =============================== load_breast_cancer ===============================
+# x_shape : (569, 30)
+# x_lda의 shape : (569, 1)
+# [1.]
+# =============================== load_digits ===============================
+# x_shape : (1797, 64)
+# x_lda의 shape : (1797, 9)
+# [0.28912041 0.47174829 0.64137175 0.75807724 0.84108978 0.90674662
+#  0.94984789 0.9791736  1.        ]
+# =============================== load_wine ===============================
+# x_shape : (178, 13)
+# x_lda의 shape : (178, 2)
+# [0.68747889 1.        ]
+# =============================== fetch_covtype ===============================
+# x_shape : (581012, 54)
+# x_lda의 shape : (581012, 6)
+# [0.72159835 0.89235761 0.94351071 0.9742032  0.99067616 1.        ]
+# ====================================================
+    
+    
+    
