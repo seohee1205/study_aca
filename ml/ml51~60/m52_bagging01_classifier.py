9938582a701c62c66d39b6ce53dbab6c7331abef
seohee1205 <dbstjgml9621@naver.com>
2023-05-01 18:04:27 +0900

    230501

--- ml/ml51~60/m52_bagging01_classifier.py
+++ ml/ml51~60/m52_bagging01_classifier.py
@@ -83,8 +83,57 @@ for i, v in enumerate(data_list):
         print('acc : ', accuracy_score(y_test, y_pred))
         
         
+# ========== 아이리스 ==========
+# 모델 이름 :  DecisionTreeClassifier
+# model.score :  0.9666666666666667
+# acc :  0.9666666666666667
+# ========== 아이리스 ==========
+# 모델 이름 :  RandomForestClassifier
+# model.score :  0.9666666666666667
+# acc :  0.9666666666666667
+# ========== 아이리스 ==========
+# 모델 이름 :  BaggingClassifier
+# model.score :  0.9666666666666667
+# acc :  0.9666666666666667
+# ========== 캔서 ==========
+# 모델 이름 :  DecisionTreeClassifier
+# model.score :  0.9298245614035088
+# acc :  0.9298245614035088
+# ========== 캔서 ==========
+# 모델 이름 :  RandomForestClassifier
+# model.score :  0.9385964912280702
+# acc :  0.9385964912280702
+# ========== 캔서 ==========
+# 모델 이름 :  BaggingClassifier
+# model.score :  0.9035087719298246
+# acc :  0.9035087719298246
+# ========== 디지트 ==========
+# 모델 이름 :  DecisionTreeClassifier
+# model.score :  0.9555555555555556
+# acc :  0.9555555555555556
+# ========== 디지트 ==========
+# 모델 이름 :  RandomForestClassifier
+# model.score :  0.9777777777777777
+# acc :  0.9777777777777777
+# ========== 디지트 ==========
+# 모델 이름 :  BaggingClassifier
+# model.score :  0.9722222222222222
+# acc :  0.9722222222222222
+# ========== 와인 ==========
+# 모델 이름 :  DecisionTreeClassifier
+# model.score :  1.0
+# acc :  1.0
+# ========== 와인 ==========
+# 모델 이름 :  RandomForestClassifier
+# model.score :  1.0
+# acc :  1.0
+# ========== 와인 ==========
+# 모델 이름 :  BaggingClassifier
+# model.score :  1.0
+# acc :  1.0
+# ========== 패치콥타입 ==========
+# 모델 이름 :  DecisionTreeClassifier
+# model.score :  0.9664724662874452
+# acc :  0.9664724662874452
 
-        
-        
-    
-    
+
