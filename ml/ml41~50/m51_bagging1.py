27c0631efe7aec79a4c8ff7925bedf43fab7958c
seohee1205 <dbstjgml9621@naver.com>
2023-05-01 17:16:44 +0900

    230501

--- ml/ml41~50/m51_bagging1.py
+++ ml/ml41~50/m51_bagging1.py
@@ -17,9 +17,19 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
 #2. 모델
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-model = DecisionTreeClassifier()
+# model = DecisionTreeClassifier()
+# model = RandomForestClassifier()
+model = BaggingClassifier(DecisionTreeClassifier(),
+                          n_estimators= 10,     # 모델을 10번 돌린다
+                          n_jobs= -1,
+                          random_state= 337,
+                        #   bootstrap= True,
+                          bootstrap = False
+                          )
+# DecisionTree를 10번 배깅한다
 
 #3. 훈련
 model.fit(x_train, y_train)
@@ -29,5 +39,14 @@ y_pred = model.predict(x_test)
 print('model.score : ', model.score(x_test, y_test))
 print('acc : ', accuracy_score(y_test, y_pred))
 
+# DecisionTree 결과
 # model.score :  0.9649122807017544
 # acc :  0.9649122807017544
+
+# RandomForest 결과
+# model.score :  0.9824561403508771
+# acc :  0.9824561403508771
+
+# Bagging 에 10번 돌린 결과
+# model.score :  0.9912280701754386
+# acc :  0.9912280701754386
