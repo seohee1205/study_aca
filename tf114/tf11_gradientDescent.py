ea8bfbd6f13a9f52c6d2190629f5f5c66b214f92
seohee1205 <dbstjgml9621@naver.com>
2023-05-12 14:43:21 +0900

    gradientDescent

---
+++ tf114/tf11_gradientDescent.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+
+x_train = [1, 2, 3]     # [1]
+y_train = [1, 2, 3]     # [2]
+
+x = tf.compat.v1.placeholder(tf.float32)
+y = tf.compat.v1.placeholder(tf.float32)
+
+w = tf.compat.v1.Variable([10], dtype= tf.float32, name = 'weight')
+
+hypothesis = x * w
+
+loss = tf.reduce_mean(tf.square(hypothesis - y))    # mse
+
+############### optimizer ###############
+lr = 0.1
+# gradient = tf.reduce_mean((w * x - y) * x)    # 이렇게 쓰면 안 돼
+gradient = tf.reduce_mean((x * w - y) * x)      # gradient의 식(미분값)
+# gradient = tf.reduce_mean((hypothesis - y) * x)
+
+descent = w - lr * gradient
+update = w.assign(descent)      # w = w - lr * gradient -> 기울기 값을 계속 업데이트
+
+##################### 옵티마이저 #####################
+w_history = []
+loss_history = []
+
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+
+for step in range(21):
+    _, loss_v, w_v = sess.run([update, loss, w], feed_dict = {x:x_train, y:y_train})
+    print(step, '\t', loss_v, '\t', w_v)
+    
+    w_history.append(w_v)
+    loss_history.append(loss_v)
+    
+sess.close()
+print('=============== w history ===============')
+print(w_history)
+print('=============== Loss history ===============')
+print(loss_history)
+
+
+
