77a4422bbdc88bd5accf62c6ed2bb3c751bb338a
seohee1205 <dbstjgml9621@naver.com>
2023-05-13 19:55:59 +0900

    dacon_book

--- competition/dacon/book2.py
+++ competition/dacon/book2.py
@@ -8,7 +8,8 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, Ro
 from sklearn.metrics import r2_score, mean_squared_error
 from tensorflow.python.keras.callbacks import EarlyStopping
 from sklearn.metrics import mean_squared_error
-
+from lightgbm import LGBMRegressor
+from xgboost import XGBRegressor
 
 #1. 데이터
 path = 'd:/study_data/_data/dacon_book/'
@@ -26,46 +27,34 @@ y = train_csv['Book-Rating']
 
 # train, test 분리
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, train_size= 0.8, random_state= 789
+    x, y, train_size= 0.8, random_state= 777
 )
-
 # print(x_train.shape, y_train.shape)  # (697114, 8) (697114,)
 # print(x_test.shape, y_test.shape)   # (174279, 8) (174279,)
 
-# Surprise 라이브러리용 Reader 및 Dataset 객체 생성
-reader = Reader(rating_scale=(0, 10))
-train = Dataset.load_from_df(train_csv[['User-ID', 'Book-ID', 'Book-Rating']], reader)
-train = train.build_full_trainset()
+categorical_cols = ['User-ID', 'Book-ID', 'Location', 'Book-Title', 'Book-Author', 'Publisher']
+x_train[categorical_cols] = x_train[categorical_cols].astype('category')
+x_test[categorical_cols] = x_test[categorical_cols].astype('category')
+test_csv[categorical_cols] = test_csv[categorical_cols].astype('category')
 
 
-# SVD 모델 훈련
-model = SVD(n_factors=200,
-            n_epochs = 1000,
-            lr_all = 0.001,
-            reg_all = 0.01)
-model.fit(train)
+model = LGBMRegressor(num_leaves= 18,
+                        max_depth= 3,
+                        learning_rate= 0.035,
+                        n_estimators= 50,
+                        subsample_for_bin= 2000,
+                        min_child_samples= 15,
+                        reg_alpha= 0.1,
+                        reg_lambda= 0.1,
+                        colsample_bytree= 0.3,
+                        subsample= 0.3
+                        )
 
-# loss = model.evaluate(x_test, y_test)
-# print('loss : ', loss)
-testset = list(zip(x_test['User-ID'], x_test['Book-ID'], x_test['Book-Rating']))
-predicted_ratings = model.test(testset)
-
-# MSE 계산
-mse = mean_squared_error([pred.r_ui for pred in predicted_ratings], [pred.est for pred in predicted_ratings])
-print("MSE:", mse)
+model.fit(x_train, y_train)
 
 y_predict = model.predict(x_test)
-def RMSE(y_test, y_predict):
-    return np.sqrt(mean_squared_error(y_test, y_predict))
-
-rmse = RMSE(y_test, y_predict)              # RMSE 함수 사용
-print("RMSE : ", rmse)
-
-# Prediction
-submit = pd.read_csv(path + 'sample_submission.csv', index_col = 0)
-
-submit['Book-Rating'] = test_csv.apply(lambda row: model.predict(row['User-ID'], row['Book-ID']).est, axis=1)
-
+mse = mean_squared_error(y_test, y_predict)
+print("RMSE : ", np.sqrt(mse))
 
 #time
 import datetime
@@ -74,5 +63,9 @@ date = date.strftime("%m%d_%H%M")
 
 # Submission
 save_path = 'd:/study_data/_save/dacon_book/'
-submit.to_csv(save_path + date + '_sample_submission.csv', index=True)
+y_submit= model.predict(test_csv)
 
+sample_submission_csv = pd.read_csv(path + 'sample_submission.csv')
+sample_submission_csv[sample_submission_csv.columns[-1]]= y_submit
+sample_submission_csv.to_csv(save_path + date + '_sample_submission.csv', index=False, float_format='%.0f')
+##
